40b9efc50336f89cbf9178222ce2bef8e38199ba
Raghav Tandon <raghavtan@gmail.com>
2025-04-28 15:27:49 +0200

    patch: exclude /helath endpoint
    -
    Signed-off-by: Raghav Tandon <raghavtan@gmail.com>

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import logging
 import sys
+import typing as t
 
 from fastapi import FastAPI, Path
 
@@ -41,3 +42,22 @@ async def finalize_generic(
 @app.get("/healthz")
 async def health_check():
     return {"status": "ok"}
+
+
+class EndpointFilter(logging.Filter):
+    def __init__(
+            self,
+            path: str,
+            *args: t.Any,
+            **kwargs: t.Any,
+    ):
+        super().__init__(*args, **kwargs)
+        self._path = path
+
+    def filter(self, record: logging.LogRecord) -> bool:
+        return record.getMessage().find(self._path) == -1
+
+
+uvicorn_logger = logging.getLogger("uvicorn.access")
+uvicorn_logger.addFilter(EndpointFilter(path="/healthz"))
+uvicorn_logger.addFilter(EndpointFilter(path="/"))
